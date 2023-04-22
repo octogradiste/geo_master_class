db54029f02612d0e69c210f7b347f3067680644d
octogradiste <colin.berger@outlook.com>
2023-04-22 15:12:24 +0200

    Keeping only cities in europe

--- script/preprocess.py
+++ script/preprocess.py
@@ -6,17 +6,20 @@ MIN_POPULATION = 10_000
 
 data = pd.read_csv(PATH, sep=';')
 
-mask = data['Population'] >= MIN_POPULATION
-data = data[mask]
+data = data[data['Population'] >= MIN_POPULATION]
+data = data[data['Timezone'].str.startswith('Europe')]
 data = data[['Name', 'Coordinates']]
 
 def normalize(row):
     coordinates = row[1].split(', ')
-    name = unidecode(row[0]).lower()
+    # name = unidecode(row[0]).lower()
+    name = row[0].lower()
     return [name, (float(coordinates[0]), float(coordinates[1]))]
 
 data = data.apply(normalize, axis=1, result_type='expand')
 data.columns = ['name', 'coordinates']
+
+print(data.info())
 
 def stringify(row):
     result = ''
