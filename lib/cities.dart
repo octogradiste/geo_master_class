01c3fdc06804c14a5408befe2426d4b5687c709d
octogradiste <colin.berger@outlook.com>
2023-04-22 15:07:40 +0200

    Playable prototype

--- lib/cities.dart
+++ lib/cities.dart
@@ -1,24 +1,24 @@
 import 'package:csv/csv.dart';
 import 'package:flutter/services.dart';
 
-class Coordinates {
+class Coordinate {
   final double latitude;
   final double longitude;
-  const Coordinates(this.latitude, this.longitude);
+  const Coordinate(this.latitude, this.longitude);
 
-  int getX(double width) {
-    return (width * (longitude + 180) / 360).round();
+  double getX(int width) {
+    return width * (longitude + 180) / 360;
   }
 
-  int getY(double height) {
-    return (height * (1 - (latitude + 90) / 180)).round();
+  double getY(int height) {
+    return height * (1 - (latitude + 90) / 180);
   }
 }
 
 class Cities {
   static var isInitialized = false;
   static const converter = CsvToListConverter(fieldDelimiter: ';', eol: '\n');
-  static late final Map<String, List<Coordinates>> cities;
+  static late final Map<String, List<Coordinate>> cities;
 
   static Future<void> initialize() async {
     if (!isInitialized) {
@@ -32,7 +32,7 @@ class Cities {
         for (var row in data)
           row[0]: [
             for (final coordinate in row[1].split(','))
-              Coordinates(
+              Coordinate(
                 double.parse(coordinate.split(':')[0]),
                 double.parse(coordinate.split(':')[1]),
               ),
--- lib/geo_master_class.dart
+++ lib/geo_master_class.dart
@@ -1,17 +1,120 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:ui' as ui;
 
-class GeoMasterClass extends StatelessWidget {
+import 'cities.dart';
+
+const EARTH_CIRCUMFERENCE = 40075; // in km
+const RADIUS = 200; // in km
+
+class GeoMasterClass extends StatefulWidget {
   const GeoMasterClass({Key? key}) : super(key: key);
+
+  @override
+  State<GeoMasterClass> createState() => _GeoMasterClassState();
+}
+
+class _GeoMasterClassState extends State<GeoMasterClass> {
+  final Set<String> cities = {};
+  final Set<Coordinate> coordinates = {};
+  final controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    Cities.initialize();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image.asset(
-          'assets/images/world5400x2700.jpg',
-          fit: BoxFit.fitHeight,
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(32),
+            child: TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+              ),
+              onChanged: (value) {
+                final city = value.toLowerCase();
+                if (!cities.contains(city) && Cities.cities.containsKey(city)) {
+                  controller.clear();
+                  setState(() {
+                    coordinates.addAll(Cities.cities[city]!);
+                    cities.add(city);
+                  });
+                }
+              },
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: FutureBuilder<ui.Image>(
+                future: loadImage(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final image = snapshot.requireData;
+                    return InteractiveViewer(
+                      child: FittedBox(
+                        fit: BoxFit.contain,
+                        child: SizedBox(
+                          width: image.width.toDouble(),
+                          height: image.height.toDouble(),
+                          child: CustomPaint(
+                            painter: CityPainter(
+                              image: image,
+                              coordinates: coordinates,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+
+  Future<ui.Image> loadImage() async {
+    final data = await rootBundle.load('assets/images/world5400x2700.jpg');
+    return decodeImageFromList(data.buffer.asUint8List());
+  }
+}
+
+class CityPainter extends CustomPainter {
+  final ui.Image image;
+  final Set<Coordinate> coordinates;
+
+  const CityPainter({
+    required this.image,
+    required this.coordinates,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawImage(image, Offset.zero, Paint());
+
+    final paint = Paint();
+    paint.color = Colors.red.withAlpha(150);
+    for (var coordinate in coordinates) {
+      final x = coordinate.getX(image.width);
+      final y = coordinate.getY(image.height);
+      final offset = Offset(x, y);
+      final radius = RADIUS * image.width / EARTH_CIRCUMFERENCE;
+      canvas.drawCircle(offset, radius, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
